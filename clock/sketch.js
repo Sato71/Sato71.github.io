fc279734393f0f3ff40ca612baf16ff435d42997
Sato71 <harusta24@gmail.com>
2025-12-09 16:16:15 +0900

    clock更新1209

--- clock/sketch.js
+++ clock/sketch.js
@@ -1,4 +1,6 @@
 let data;
+//背景　先生
+let sky;
 
 let week = [
   '日',//0番
@@ -10,16 +12,41 @@ let week = [
   '土',
 ];
 
-async function preload(){
-  getData();
-}
+// async function preload(){
+//   getData();
+// }
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  getData();//天気の取得開始
+
+  // let dog; // 犬
+  // console.log("リクエスト開始。");
+  // loadJSON("https://dog.ceo/api/breeds/image/random", function(data) { // コールバック関数
+  //   console.log("データ取得完了:", data);
+  //   dog = data; // 取得データを明け渡す
+  // });
+  // console.log("データ取得中...");
 }
 
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+
 function draw() {
-  background(220);
+  //時間で背景の色が移り変わる。自作
+  // let h = hour();
+  // let c = map(h, 0, 23, 0, 200);
+  // background(0, c, 200);
+  
+  //背景　先生
+  loadImage('sky.png', newImg =>{
+
+  });
+
+  let cx = width / 2;
+  let cy = height / 2;
 
   //translate(0, 40);//キャンバスをずらす
   //textAlign(CENTER);
@@ -31,26 +58,37 @@ function draw() {
   let day = date.getDate();//現在の日
   let dow = date.getDay();//現在の曜日
 
-  let hour = date.getHours();//現在の時
+  let currenthour = date.getHours();//現在の時
   let minute = date.getMinutes();//現在の分
   let second = date.getSeconds();//現在の秒
 
+  //背景　先生
+  if(sky){
+    let skyColor = sky.get(h * 10, 10);
+    background(skyColor);
+  }
+
+  textAlign(CENTER);
   textSize(30);
   textStyle(BOLD);
-
-  text(year + '/' + month + '/' + day + '(' + week[dow] + ')', 70, 150);
-  text(hour + ':' + minute + ':' + second, 110, 250);
+  text(year + '/' + month + '/' + day + '(' + week[dow] + ')', cx, cy - 40);
+  text(currenthour + ':' + minute + ':' + second, cx, cy);
   
-  //温度
-  text(data.current.temperature_2m + '度', 120, 300);
+  if(data){//データが取得済みなら
+   //温度
+   text(data.current.temperature_2m + '度', cx, cy + 60);
+  }
+  
 
 }
 //draw関数でlet dataを呼び出しちゃダメ！！　天気情報くれって1秒に60回60回言うことになっちゃう。
 
 
 
-//天気情報を取得
-async function getData(){
-  data = await loadJSON('https://api.open-meteo.com/v1/forecast?latitude=36.5667&longitude=139.8833&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m,dew_point_2m,precipitation_probability,rain,weather_code&current=temperature_2m,relative_humidity_2m,is_day&timezone=Asia%2FTokyo&forecast_days=1')
-  console.log(data);
+//天気情報を取得　自作の関数
+function getData(){
+  loadJSON('https://api.open-meteo.com/v1/forecast?latitude=36.5667&longitude=139.8833&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m,dew_point_2m,precipitation_probability,rain,weather_code&current=temperature_2m,relative_humidity_2m,is_day&timezone=Asia%2FTokyo&forecast_days=1', newData => {
+    data = newData;//データ取得完了
+    console.log(data);
+  });
 }
