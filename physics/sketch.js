b249e672b2ce03e97b6f169b0effc28baba2094e
Sato71 <harusta24@gmail.com>
2025-12-16 16:10:48 +0900

    物理演算

---
+++ physics/sketch.js
@@ -0,0 +1,43 @@
+let { Engine, Bodies, Composite } = Matter;//モジュールを変数化
+let engine;//物理演算用の空間
+
+function setup() {
+  createCanvas(400, 400);
+
+  engine = Engine.create();
+
+  //箱を生成
+  let boxA = Bodies.rectangle(150, 200, 120, 120);//x, y, 幅, 高さ
+  let boxB = Bodies.rectangle(200, 0, 80, 80);
+  let ball = Bodies.circle(120, 30, 50);
+  let ground = Bodies.rectangle(200, 350, 380, 50, {isStatic: true});
+
+  //箱を世界に配置
+  Composite.add(engine.world, [boxA, boxB, ball, ground]);
+}
+
+function draw() {
+  background(220);
+
+  //世界に配置された全ての物体を取得（配列）
+  let bodies = Composite.allBodies(engine.world);
+
+  for(let i = 0; i < bodies.length; i++){
+    drawBody(bodies[i]);
+  }
+
+  //世界の更新(1フレーム時間を進める)
+  Engine.update(engine,deltaTime);
+
+
+}
+
+//自作関数:　引数で渡された物体を描画する
+function drawBody(body){
+  let v = body.vertices;//物体の頂点（配列）
+  beginShape();//多角形描画開始
+  for (let i = 0; i < v.length; i++){
+    vertex(v[i].x, v[i].y);
+  }
+  endShape(CLOSE);//多角形描画終了
+}
